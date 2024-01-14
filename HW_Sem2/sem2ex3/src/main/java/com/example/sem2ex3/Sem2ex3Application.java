3cf176cea360ecdde4ce78dd563e291f551c01a6
Vladislav <vlad-pugovkin2@tandex.ru>
2024-01-14 18:09:48 +0300

    Add HW_SEM2 Tasks 1-3

---
+++ HW_Sem2/sem2ex3/src/main/java/com/example/sem2ex3/Sem2ex3Application.java
@@ -0,0 +1,13 @@
+package com.example.sem2ex3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem2ex3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem2ex3Application.class, args);
+	}
+
+}
---
+++ HW_Sem2/sem2ex3/src/test/java/com/example/sem2ex3/Sem2ex3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sem2ex3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem2ex3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
