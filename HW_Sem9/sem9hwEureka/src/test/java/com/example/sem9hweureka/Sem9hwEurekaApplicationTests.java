98554fe53b901b25a87680e394d4e5c9c7822060
Vladislav <vlad-pugovkin2@tandex.ru>
2024-02-13 15:56:14 +0300

    Add HW_Sem9 Task2 Eureka

---
+++ HW_Sem9/sem9hwEureka/src/test/java/com/example/sem9hweureka/Sem9hwEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sem9hweureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem9hwEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
